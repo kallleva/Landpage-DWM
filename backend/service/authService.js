b72f70d1c0852a018b6668923122a491587553bf
kalleb vieira <kallebv543@gmail.com>
2024-11-01 00:02:46 -0300

    testando error

--- backend/service/authService.js
+++ backend/service/authService.js
@@ -1,35 +1,35 @@
-// backend/service/authService.js
+// // backend/service/authService.js
 
-const userRepository = require('../repository/userRepository');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+// const userRepository = require('../repository/userRepository');
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcryptjs');
 
-exports.login = async (username, password) => {
-  if (!username || !password) {
-    throw new Error('Username and password must be provided');
-  }
+// exports.login = async (username, password) => {
+//   if (!username || !password) {
+//     throw new Error('Username and password must be provided');
+//   }
 
-  const user = await userRepository.findByUsername(username);
-  if (user) {
-    // Comparar a senha fornecida com a senha hashada no banco de dados
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (isPasswordValid) {
-      // Definir o payload do token
-      const payload = {
-        id: user.id,
-        username: user.username,
-        // Adicione outros campos se necessário
-      };
+//   const user = await userRepository.findByUsername(username);
+//   if (user) {
+//     // Comparar a senha fornecida com a senha hashada no banco de dados
+//     const isPasswordValid = await bcrypt.compare(password, user.password);
+//     if (isPasswordValid) {
+//       // Definir o payload do token
+//       const payload = {
+//         id: user.id,
+//         username: user.username,
+//         // Adicione outros campos se necessário
+//       };
 
-      // Gerar o token JWT
-      const token = jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn: '1h', // O token expira em 1 hora
-      });
+//       // Gerar o token JWT
+//       const token = jwt.sign(payload, process.env.JWT_SECRET, {
+//         expiresIn: '1h', // O token expira em 1 hora
+//       });
 
-      return { token };
-    }
-  }
+//       return { token };
+//     }
+//   }
 
-  // Se as credenciais forem inválidas, retorne null
-  return null;
-};
+//   // Se as credenciais forem inválidas, retorne null
+//   return null;
+// };
