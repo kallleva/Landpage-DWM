8f07c5dff45feae77ecb8d3357c67583119a2eea
kalleb vieira <kallebv543@gmail.com>
2024-11-01 11:14:22 -0300

    revert no index teste

--- app/index.js
+++ app/index.js
@@ -39,7 +39,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 // Rota padrão que serve o 'index.html'
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'app', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 // Inicia o servidor
 app.listen(PORT, () => {
