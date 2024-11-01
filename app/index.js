e410a1634c71e3f26a87722314f45b92f424503c
kalleb vieira <kallebv543@gmail.com>
2024-11-01 10:36:48 -0300

    revert no index teste

--- app/index.js
+++ app/index.js
@@ -1,27 +1,47 @@
+// const express = require('express');
+// const path = require('path');
+// require('dotenv').config();
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// // Middleware para servir arquivos estáticos do frontend
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Middleware para parsear JSON
+// app.use(express.json());
+
+// // Importar e usar rotas do backend
+// const apiRoutes = require('./backend/routers/api');
+// app.use('/api', apiRoutes);
+
+// // Rota para servir o frontend (SPA - Single Page Application)
+// app.get('/', (req, res) => {
+//   // Fornecendo o diretório raiz explicitamente
+//   res.sendFile('index.html', { root: path.join(__dirname, 'public') });
+// });
+
+// // Iniciar o servidor
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+
+
+
+// app.js
 const express = require('express');
 const path = require('path');
-require('dotenv').config();
-
 const app = express();
-const port = process.env.PORT || 3000;
-
-// Middleware para servir arquivos estáticos do frontend
+// Use a porta fornecida pelo ambiente, se não estiver disponível use a porta 3000
+const PORT = process.env.PORT || 3000;
+// Serve arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Middleware para parsear JSON
-app.use(express.json());
-
-// Importar e usar rotas do backend
-const apiRoutes = require('./backend/routers/api');
-app.use('/api', apiRoutes);
-
-// Rota para servir o frontend (SPA - Single Page Application)
+// Rota padrão que serve o 'index.html'
 app.get('/', (req, res) => {
-  // Fornecendo o diretório raiz explicitamente
-  res.sendFile('index.html', { root: path.join(__dirname, 'public') });
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-// Iniciar o servidor
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
