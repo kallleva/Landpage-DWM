5ad7fbcd4dc787af0566b6e2c8322b32f5447997
kalleb vieira <kallebv543@gmail.com>
2024-11-01 00:58:55 -0300

    teste

--- index.js
+++ index.js
@@ -18,8 +18,20 @@ app.use('/api', apiRoutes);
 
 // Rota para servir o frontend (SPA - Single Page Application)
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  const filePath = path.join(__dirname, 'public', 'index.html');
+  console.log('Tentando enviar arquivo:', filePath);
+  try {
+    res.sendFile(filePath);
+  } catch (error) {
+    console.error('Erro ao enviar o arquivo:', error);
+    res.status(500).send('Erro ao enviar o arquivo');
+  }
 });
+
+app.get('/test', (req, res) => {
+  res.send('Servidor está funcionando!');
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
